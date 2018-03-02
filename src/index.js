5448d7a479a0ac0907aa999eed10eadc1ab34d88
VLanc <vlancv@gmail.com>
2018-03-02 16:49:09 +0300

    Task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let result = 0;
+  let primesArr = []; 
+  let maxPrime;
+
+  // search all primes in the range from 2 to base
+  nextPrime:
+  for (let i = 2; i <= base; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+    primesArr.push(i);
+  }
+
+  // search maximum prime
+  for(let i = primesArr.length - 1; i < primesArr.length; i--) {
+    if(base % primesArr[i] == 0) {
+      maxPrime = primesArr[i];
+      break;
+    }
+  }
+
+  while(number >= maxPrime) {
+    number = Math.floor(number/maxPrime);
+    result += number;
+  }
+
+  return result;
 }
