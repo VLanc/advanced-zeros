e943e6813333179da0d7c70d87c0e4e5fa383bed
VLanc <vlancv@gmail.com>
2018-03-03 19:35:35 +0300

    Task completed by 100 points

--- src/index.js
+++ src/index.js
@@ -1,29 +1,42 @@
 module.exports = function getZerosCount(number, base) {
-  let result = 0;
+  let result;
   let primesArr = []; 
-  let maxPrime;
+  let degrees = {};
+  let numberCopy = number;
 
-  // search all primes in the range from 2 to base
   nextPrime:
   for (let i = 2; i <= base; i++) {
     for (let j = 2; j < i; j++) {
       if (i % j == 0) continue nextPrime;
     }
     primesArr.push(i);
+    degrees[i] = 0;
   }
-
-  // search maximum prime
-  for(let i = primesArr.length - 1; i < primesArr.length; i--) {
+  
+  for(let i = primesArr.length - 1; i >= 0; i--) {
     if(base % primesArr[i] == 0) {
-      maxPrime = primesArr[i];
-      break;
+      degrees[primesArr[i]]++;
+      base /= primesArr[i];
+      i++;
     }
   }
 
-  while(number >= maxPrime) {
-    number = Math.floor(number/maxPrime);
-    result += number;
+  let resultArr = [];
+  let elementNumber = 0;
+  
+  for(let key in degrees) {
+    if(degrees[key] != 0) {
+      resultArr.push(0);
+      while(numberCopy >= key) {
+        numberCopy = Math.floor(numberCopy / key);
+        resultArr[elementNumber] += numberCopy;
+      }
+      resultArr[elementNumber] /= degrees[key];
+      elementNumber++;
+      numberCopy = number;
+    }
   }
 
+  result = Math.floor(Math.min.apply(null, resultArr));
   return result;
 }
